0366ab3b729db4a00043dec73086adadc002d9a0
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-05-27 22:36:55 +0200

    styled-components

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import logo from "../assets/images/logo.svg";
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/LandingPage";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            Job <span>Track</span> App
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur,
+            quae eum nulla qui deserunt voluptatem aliquid ullam, neque eos
+            voluptatum nisi autem voluptates expedita necessitatibus mollitia,
+            soluta laudantium dicta! Placeat!
+          </p>
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn ">
+            Login / Demo User
+          </Link>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Landing;
