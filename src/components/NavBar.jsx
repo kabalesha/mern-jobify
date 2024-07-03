ea7299f44dba87cbcac1aab891223e514de34cb1
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-07-03 20:34:29 +0200

    react-icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,12 @@
 import Wrapper from "../assets/wrappers/Navbar";
-
+import { IoMdHome } from "react-icons/io";
 const NavBar = () => {
-  return <Wrapper>NavBar</Wrapper>;
+  return (
+    <Wrapper>
+      <h2>NavBar</h2>
+      <IoMdHome />
+    </Wrapper>
+  );
 };
 
 export default NavBar;
