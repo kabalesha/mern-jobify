ed711bbd743e6fb41545bf0e57f14050e1d0dedc
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2024-07-05 22:02:40 +0200

    sidebar structure

--- src/components/SmallSideBar.jsx
+++ src/components/SmallSideBar.jsx
@@ -1,10 +1,37 @@
+import { FaTimes } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import { useDashboardContext } from "../pages/DashboardLayout";
+import links from "../utils/links";
+import Logo from "./Logo";
 
 const SmallSidebar = () => {
   const data = useDashboardContext();
-  console.log(data);
-  return <Wrapper>SmallSidebar</Wrapper>;
+  return (
+    <Wrapper>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button type="button" className="close-btn">
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+            <div className="nav-links">
+              {links.map((link) => {
+                const { text, path, icon } = link;
+                return (
+                  <NavLink to={path} key={text} className="nav-link">
+                    <span className="icon">{icon}</span>
+                    {text}
+                  </NavLink>
+                );
+              })}
+            </div>
+          </header>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
--- src/utils/links.jsx
+++ src/utils/links.jsx
@@ -30,3 +30,5 @@ const links = [
     icon: <MdAdminPanelSettings />,
   },
 ];
+
+export default links;
